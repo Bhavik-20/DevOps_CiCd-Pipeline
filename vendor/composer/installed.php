2c930a7ae9715ce4270251debdcf0f88e280a9cc
Bhavik-20 <59500727+Bhavik-20@users.noreply.github.com>
2021-11-03 15:50:44 +0530

    lock file added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'bf65dbe4f69090dd579d15da2adc28ca821e21c6',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'bf65dbe4f69090dd579d15da2adc28ca821e21c6',
+            'dev_requirement' => false,
+        ),
+    ),
+);
